76793a16aaffa93fe561f465513f1f00b1ad2f0f
Karl Linderhed <git@karlinde.se>
2020-07-04 18:59:49 +0200

    Fixed improper nginx configuration for mediawiki in https, leading to possible exposure of settings file. New passwords for mediawiki as a precaution

--- site-modules/mediawiki/manifests/init.pp
+++ site-modules/mediawiki/manifests/init.pp
@@ -145,49 +145,80 @@ class mediawiki (
   include php::fpm
 
   nginx::resource::server { $domain:
-    www_root     => '/var/www/mediawiki/',
-    index_files  => ['index.php'],
-    require      => Class['::base::certificates'],
-    ssl_redirect => true,
-    ssl          => true,
-    ssl_cert     => '/etc/letsencrypt/live/all-sites/fullchain.pem',
-    ssl_key      => '/etc/letsencrypt/live/all-sites/privkey.pem',
+    autoindex            => 'off',
+    index_files          => ['index.php'],
+    www_root             => '/var/www/mediawiki/',
+    require              => Class['::base::certificates'],
+    ssl_redirect         => true,
+    ssl                  => true,
+    use_default_location => false,
+    ssl_cert             => '/etc/letsencrypt/live/all-sites/fullchain.pem',
+    ssl_key              => '/etc/letsencrypt/live/all-sites/privkey.pem',
   }
 
-  nginx::resource::location{'mediawiki':
-    server             => $domain,
-    www_root           => '/var/www/mediawiki/',
-    location           => '~ \.php$',
-    fastcgi_param      => {
-      'SCRIPT_FILENAME' => '$document_root$fastcgi_script_name',
-    },
-    fastcgi_split_path => '^(.+\.php)(/.+)$',
-    fastcgi            => '127.0.0.1:9000',
-    index_files        => [],
-  }
-
-  nginx::resource::location { 'files':
-    location    => '~* \.(js|css|png|jpg|jpeg|gif|ico)$',
-    www_root    => '/var/www/mediawiki/',
+  nginx::resource::location{'/':
     server      => $domain,
     index_files => [],
+    try_files   => ['$uri', '$uri/', '/index.php'],
+      ssl       => true,
+      ssl_only  => true,
   }
 
-  nginx::resource::location { 'maintenance':
-    location            => '^~ /maintenance/',
-    server              => $domain,
-    index_files         => [],
-    location_custom_cfg => {
-      'return' => '403',
+  nginx::resource::location{'~ \.php$':
+    server        => $domain,
+    index_files   => [],
+    fastcgi_param => {
+      'SCRIPT_FILENAME' => '$document_root$fastcgi_script_name',
     },
+    include       => ['fastcgi_params'],
+    fastcgi       => '127.0.0.1:9000',
+    ssl           => true,
+    ssl_only      => true,
   }
 
-  nginx::resource::location { 'denial':
-    location            => '^~ ^/(cache|includes|maintenance|languages|serialized|tests|images/deleted)/',
+  nginx::resource::location { '~* \.(js|css|png|jpg|jpeg|gif|ico)$':
+    server      => $domain,
+    index_files => [],
+    try_files   => ['$uri', '/index.php'],
+    expires     => 'max',
+    ssl         => true,
+    ssl_only    => true,
+  }
+
+  nginx::resource::location { '^~ ^/(cache|includes|maintenance|languages|serialized|tests|images/deleted)/':
     server              => $domain,
     index_files         => [],
+    ssl                 => true,
+    ssl_only            => true,
     location_custom_cfg => {
       'deny' => 'all',
     },
+  }
+
+  nginx::resource::location { '^~ ^/(bin|docs|extensions|includes|maintenance|mw-config|resources|serialized|tests)/':
+    server      => $domain,
+    index_files => [],
+    internal    => true,
+    ssl         => true,
+    ssl_only    => true,
+  }
+
+  nginx::resource::location { '~* ^/images/.*\.(html|htm|php|shtml)$':
+    server              => $domain,
+    index_files         => [],
+    ssl                 => true,
+    ssl_only            => true,
+    #location_cfg_append => 'types { }',
+    location_custom_cfg => {
+      'default_type' => 'text/plain',
+    },
+  }
+
+  nginx::resource::location { '^~ /images/':
+    server      => $domain,
+    index_files => [],
+    try_files   => ['$uri', '/index.php'],
+    ssl         => true,
+    ssl_only    => true,
   }
 }
