580e910bff75352ca353d4b6f29494dedcda4639
Karl Linderhed <git@karlinde.se>
2020-07-04 10:52:07 +0000

    Fixed cron job clutter and smtp client settings

--- puppet-update.sh
+++ puppet-update.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Updates the puppet code and runs the update script, which runs puppet apply.
 
-cd /etc/puppet/code
+cd /etc/puppet/code || exit 1
 
-git pull || exit 1
-./update.sh || exit 1
+git pull > /dev/null || exit 1
+
+# Until postgresql_password deprecation fixes have landed, redirect stderr as well to avoid clutter in logs and email
+./update.sh &> /dev/null || exit 1
