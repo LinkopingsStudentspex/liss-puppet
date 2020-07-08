18e14acafd384c976e6ab4ef6da9eb228fc7dc21
Karl Linderhed <git@karlinde.se>
2020-07-08 17:05:34 +0200

    Fixed backup jobs for postgres not being run in cron

--- site-modules/backup/files/backup_db_postgres.sh
+++ site-modules/backup/files/backup_db_postgres.sh
@@ -3,4 +3,4 @@ db_name=$1
 location=$2
 recipient=$3
 timestamp=$(date +"%Y-%m-%d_%H%M%S")
-runuser -u postgres -- pg_dump -Fc $db_name | gpg --encrypt --output $location/${db_name}_$timestamp.dump.gpg -r $recipient
+/sbin/runuser -u postgres -- pg_dump -Fc $db_name | gpg --encrypt --output $location/${db_name}_$timestamp.dump.gpg -r $recipient
