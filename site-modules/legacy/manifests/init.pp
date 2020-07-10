3d5627f300c677f695876eb2a69cb01e39a9f84d
Karl Linderhed <git@karlinde.se>
2020-07-10 12:41:08 +0200

    Added spexflix subdomain to legacy oidc client

--- site-modules/legacy/manifests/init.pp
+++ site-modules/legacy/manifests/init.pp
@@ -3,11 +3,12 @@ class legacy (
   $oidc_clientid = 'old_site',
   $oidc_clientsecret = '',
   $domain = '',
+  $spexflix_domain = '',
 ) {
   keycloak_client { $oidc_clientid:
     realm                 => 'liss',
     secret                => $oidc_clientsecret,
-    redirect_uris         => ["https://${domain}/*"],
+    redirect_uris         => ["https://${domain}/*", "https://${spexflix_domain}/*"],
     default_client_scopes => ['profile', 'email', 'member_number'],
   }
 }
