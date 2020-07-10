5ae8ad00e48e2ea9aa45ddc0dfacb78d3ff651e7
Karl Linderhed <git@karlinde.se>
2020-07-10 22:23:04 +0200

    Fix warning in cron when running backups

--- site-modules/backup/files/backup_db_postgres.sh
+++ site-modules/backup/files/backup_db_postgres.sh
@@ -3,4 +3,4 @@ db_name=$1
 location=$2
 recipient=$3
 timestamp=$(date +"%Y-%m-%d_%H%M%S")
-/sbin/runuser -u postgres -- pg_dump -Fc $db_name | gpg --encrypt --output $location/${db_name}_$timestamp.dump.gpg -r $recipient
+/sbin/runuser -l postgres -c "pg_dump -Fc $db_name" | gpg --encrypt --output $location/${db_name}_$timestamp.dump.gpg -r $recipient
