c05c7d8674e00ba688cccbe25d89664aa8184bfc
Karl Linderhed <git@karlinde.se>
2020-07-10 21:08:31 +0200

    Added a user for running the export script over ssh

--- site-modules/internsidor/manifests/init.pp
+++ site-modules/internsidor/manifests/init.pp
@@ -37,4 +37,17 @@ class internsidor (
     content => epp('internsidor/settings/local.py.epp'),
     notify  => Service['internsidor-gunicorn'],
   }
+
+  # Create and manage a user that can connect from the old system
+  user {'lisse':
+    home => '/home/lisse',
+  }
+
+  # Only allow lisse to run the sync/export script from ssh
+  ssh_authorized_key {'lisse-export':
+    type    => 'ssh-rsa',
+    key     => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDCN+sK0tX9K1hteFRnykLqxwsyHLjhEvsMgMR/uEyTNx2+aUwtD33sM6+4eyQYviNWb1lvQaPQN9tFVZPEMH/25+l/X3uUKlyIrW3qveoVZlVy1bzOoEHIBKStccFUhFIiKjqyHQXMIk8aBIElPXJkd//kLhkQqVZEjHY3e8pPuKt4ETQgiodVwefnVTAkTeOBMFWFmAAdTas8jB35QCgpOVHVaq1allEcm2Jl6ok/Vy85ds4Vc0m2ijs1tDrCIye9KrU7rrENKxgsBTUsRTChPfUVh1Zt4LSRCOEDCha/Wuh90CKhssuW2BmN3+mYWNS6VykIqKm+uhMTzVJGyIt1O86O/VTUFjV4V9gCaXS40WCFRQEuNghTLeRfm+fO9I8NHYB4Fl4ua82ztmkMS9xiwOcLd36cLtTtlZJPVscrLmSdV6C5spm2tru8jetRwqK14X55uon63VikxacsXytHp7S/MABolMfqupG7JAZx4z7ItotFXkaLugGGQLISGPUTQ9HoXUrzDDQbg1uSgpZZzL5hoM80yTTNkyGCBt6ot5d2+omY9DaYbLNnBD2dO6a3cjiaQJ8QS9O4hEblP2Cwu/9vBcVUSua3b+GKQMYrZs6Jj65/SGKudTuObdwpqop1oIfi+WPVyriZu4otbNacYkg2SG13MZdCAKWPC9/xwQ==',
+    user    => 'lisse',
+    options => ["command=\"PYTHONIOENCODING='utf8' ${venv_path}/bin/python ${project_path}/legacy_export.py\""],
+  }
 }
