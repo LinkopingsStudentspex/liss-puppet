e6d1caa7ac17bf636b046e426935fc35385286b0
Karl Linderhed <git@karlinde.se>
2021-07-01 20:02:18 +0200

    Update mail config for outgoing relay via LiU
    Also some cleanup of the config file.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,5 @@
 $organization_domain = lookup('organization_domain')
 $smtp_host = lookup('smtp_host')
-$smtp_user = lookup('smtp_user')
 $smtp_password = lookup('smtp_password')
 $smtp_port = lookup('smtp_port')
 $keycloak_auth_url = lookup('keycloak_auth_url')
